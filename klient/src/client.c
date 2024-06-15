06edb2f73f859f55b83451642cc495d346255114
JakubMlocek <jakub.mlocek.informatyka@gmail.com>
2024-06-15 09:10:35 +0200

    splited client into smaller functions

--- klient/src/client.c
+++ klient/src/client.c
@@ -10,52 +10,11 @@
 #define BOARD_SIZE 8
 
 
-int main() {
-    int sockfd;
-    struct sockaddr_in server_addr;
-
-    // Create client socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (sockfd < 0) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-
-	int optval = 1;	
-	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
-
-    // Set server address
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
-
-    // Connect to server
-    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Connection failed");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Connected to server at %s:%d\n", SERVER_IP, PORT);
-
+void play_game(int sockfd, char player_char){
     char board[BOARD_SIZE][BOARD_SIZE];
-    
     initialize_board(board);
-    
-    
-    //Odbieramy informacje od serwera, ktorym graczem jestesmy.
-    char player_char;
-
-    if (recv(sockfd, &player_char, BUFFER_SIZE, 0) < 0) {
-        perror("recv failed");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Jestes graczem: %c\n", player_char);
-
-    // Main game loop
     char *buffer = malloc(BUFFER_SIZE);
+
 
     while(1){
         // odbieranie planszy z serwera:
@@ -67,7 +26,7 @@ int main() {
         set_board_to_buffer(board, buffer);
         print_board(board);
         make_move(board, player_char);
-
+        print_board(board);
         buffer = get_buffer_from_board(board);   
         // Wysylanie buforu z planszy do serwera
         if (send(sockfd, buffer, BUFFER_SIZE, 0) < 0) {
@@ -75,6 +34,45 @@ int main() {
             exit(EXIT_FAILURE);
         }
     }
+}
+
+int main() {
+    int sockfd;
+    struct sockaddr_in server_addr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+	int optval = 1;	
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connected to server at %s:%d\n", SERVER_IP, PORT);
+    
+    //Odbieramy informacje od serwera, ktorym graczem jestesmy.
+    char player_char;
+    if (recv(sockfd, &player_char, BUFFER_SIZE, 0) < 0) {
+        perror("recv failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Jestes graczem: %c\n", player_char);
+
+    //rozpoczynamy gre
+    play_game(sockfd, player_char);
+
     close(sockfd);
     return 0;
 }
