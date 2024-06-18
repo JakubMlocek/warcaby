f136ebbfd8aab523fa732d58feb9a8098c9e56a9
JakubMlocek <jakub.mlocek.informatyka@gmail.com>
2024-06-18 21:24:27 +0200

    directories changes

---
+++ olds/TLV_format/game.h
@@ -0,0 +1,20 @@
+
+#ifndef GAME_H
+#define GAME_H
+
+#define BOARD_SIZE 8
+#define BOARD_SIZE 8
+#define BUFFER_SIZE 1024
+
+
+void initialize_board(char board[BOARD_SIZE][BOARD_SIZE]);
+
+void print_board(char board[BOARD_SIZE][BOARD_SIZE]);
+
+void make_move(char board[BOARD_SIZE][BOARD_SIZE], char player);
+
+char* get_buffer_from_board(char board[BOARD_SIZE][BOARD_SIZE]);
+
+void set_board_to_buffer(char board[BOARD_SIZE][BOARD_SIZE], char *buffer);
+
+#endif // GAME_H
